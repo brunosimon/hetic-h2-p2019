b985dc4159056a361520f7a1c1a971285e4a9866
Bruno Simon <simon.bruno.77@gmail.com>
2016-01-24 16:54:09 +0100

    Update PHP lessons + Add PHP Formulaire lesson

--- exercices/cours-14-carousel-objet-G1/src/js/app/script.js
+++ exercices/cours-14-carousel-objet-G1/src/js/app/script.js
@@ -1,24 +1,74 @@
 var Carousel = function( $target )
 {
-	console.log($target);
+	this.$                  = {};
+	this.$.container        = $target;
+	this.$.prev             = this.$.container.find( '.prev' );
+	this.$.next             = this.$.container.find( '.next' );
+	this.$.slides_container = this.$.container.find( '.slides .items' );
+	this.$.slides           = this.$.slides_container.find( '.item' );
+
+	this.count = this.$.slides.length;
+
+	this.init_events();
 };
 
-Carousel.prototype.index = 0;
 Carousel.prototype.count = 0;
+Carousel.prototype.index = 0;
 
-Carousel.prototype.next = function()
+Carousel.prototype.init_events = function()
 {
+	var that = this;
 
+	this.$.next.on( 'click', function()
+	{
+		that.next();
+
+		return false;
+	} );
+
+	this.$.prev.on( 'click', function()
+	{
+		that.prev();
+
+		return false;
+	} );
 };
 
 Carousel.prototype.prev = function()
 {
-	
+	this.go_to( this.index - 1 );
+};
+
+Carousel.prototype.next = function()
+{
+	this.go_to( this.index + 1 );
 };
 
 Carousel.prototype.go_to = function( index )
 {
-	
+	// Clamp
+	if( index < 0 )
+		index = 0;
+	else if( index > this.count - 1 )
+		index = this.count - 1;
+
+	// Animer
+	this.$.slides_container.animate( {
+		left : - index * 600
+	} );
+
+	this.index = index;
 };
 
-var carousel = new Carousel( $( '.carousel' ) );
+
+
+
+var $carousels = $( '.carousel' );
+
+$carousels.each( function()
+{
+	var $carousel = $( this ),
+		carousel  = new Carousel( $carousel );
+} );
+
+
--- exercices/cours-14-carousel-objet-G2/src/js/app/script.js
+++ exercices/cours-14-carousel-objet-G2/src/js/app/script.js
@@ -1,1 +1,69 @@
-console.log('yo');
+var Carousel = function( $target )
+{
+	this.$                  = {};
+	this.$.container        = $target;
+	this.$.next             = this.$.container.find( '.next' );
+	this.$.prev             = this.$.container.find( '.prev' );
+	this.$.slides_container = this.$.container.find( '.items' );
+	this.$.slides           = this.$.slides_container.find( '.item' );
+
+	this.count = this.$.slides.length;
+
+	this.init_events();
+};
+
+Carousel.prototype.count = 0;
+Carousel.prototype.index = 0;
+
+Carousel.prototype.init_events = function()
+{
+	var that = this;
+
+	this.$.next.on( 'click', function()
+	{
+		that.next();
+
+		return false;
+	} );
+
+	this.$.prev.on( 'click', function()
+	{
+		that.prev();
+
+		return false;
+	} );
+};
+
+Carousel.prototype.next = function()
+{
+	this.go_to( this.index + 1 );
+};
+
+Carousel.prototype.prev = function()
+{
+	this.go_to( this.index - 1 );
+};
+
+Carousel.prototype.go_to = function( index )
+{
+	// Clamp
+	if( index < 0 )
+		index = 0;
+	else if( index > this.count - 1 )
+		index = this.count - 1;
+
+	// Animate
+	this.$.slides_container.animate( {
+		left : - 600 * index
+	} );
+
+	this.index = index;
+};
+
+var $carousels = $( '.carousel' );
+
+$carousels.each( function()
+{
+	var $carousel = $( this ),
+		carousel  = new Carousel( $carousel );
+} );
